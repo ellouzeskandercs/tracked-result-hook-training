f06dcc918b7602d5cff26847ad5fd93493477695
Skander Ellouze <skandere@bam.tech>
2023-12-25 22:20:16 +0100

    feat(hooks): create number of renders hooks

--- src/test-query/useNbOfRenders.js
+++ src/test-query/useNbOfRenders.js
@@ -1,1 +1,7 @@
-export const useNbOfRenders = () => {};
+import { useRef } from "react";
+
+export const useNbOfRenders = () => {
+  const nbOfRenders = useRef(0);
+  nbOfRenders.current = nbOfRenders.current + 1;
+  return nbOfRenders.current;
+};
